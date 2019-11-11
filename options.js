70ad7f1cf296e8d089e762792283cd0b320cc807
magnolia1234 <54500520+magnolia1234@users.noreply.github.com>
2019-11-11 18:16:09 +0100

    Manual rebase

--- options.js
+++ options.js
@@ -15,6 +15,7 @@ var defaultSites = {
   'De Volkskrant': 'volkskrant.nl',
   'DeMorgen': 'demorgen.be',
   'Denver Post': 'denverpost.com',
+  'Die Zeit': 'zeit.de',
   'Dynamed Plus': 'dynamed.com',
   'Encyclopedia Britannica': 'britannica.com',
   'Eindhovens Dagblad': 'ed.nl',
@@ -30,11 +31,11 @@ var defaultSites = {
   'Hartford Courant': 'courant.com',
   'Harvard Business Review': 'hbr.org',
   'Inc.com': 'inc.com',
-  'Irish Times': 'irishtimes.com',
   'La Nacion': 'lanacion.com.ar',
   'La Repubblica': 'repubblica.it',
   'La Tercera': 'latercera.com',
   'L\'Echo': 'lecho.be',
+  'Le Devoir': 'ledevoir.com',
   'Le Monde': 'lemonde.fr',
   'Le Parisien': 'leparisien.fr',
   'Les Echos': 'lesechos.fr',
@@ -69,8 +70,11 @@ var defaultSites = {
   'The Australian Financial Review (javascript disabled)': 'afr.com',
   'The Boston Globe (javascript disabled)': 'bostonglobe.com',
   'The Business Journals (javascript disabled)': 'bizjournals.com',
+  'The Canberra Times': 'canberratimes.com.au',
   'The Economist (javascript disabled)': 'economist.com',
   'The Globe and Mail (javascript disabled)': 'theglobeandmail.com',
+  'The Hindu': 'thehindu.com',
+  'The Irish Times': 'irishtimes.com',
   'The Japan Times': 'japantimes.co.jp',
   'TheMarker': 'themarker.com',
   'The Mercury News': 'mercurynews.com',
@@ -93,8 +97,7 @@ var defaultSites = {
   'Winston-Salem Journal': 'journalnow.com',
   'Vanity Fair': 'vanityfair.com',
   'Vrij Nederland': 'vn.nl',
-  'Wired': 'wired.com',
-  'Zeit Online': 'zeit.de'
+  'Wired': 'wired.com'
 };
 
 // Saves options to chrome.storage
