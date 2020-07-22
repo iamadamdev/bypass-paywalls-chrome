f0bcd85d536cad42817005a2f22e13996516e926
Ben Burns <ben.s.burns@gmail.com>
2020-07-21 18:05:41 -0600

    Add logic against certain race conditions

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -348,11 +348,25 @@ if (matchDomain('estadao.com.br')) {
     const subscriberOnly = document.querySelectorAll('.subscriber-only.hide');
 
     if (paywallModal) {
-      removeDOMElement(paywallModal, modalBackdrop, loginLink, redactedOverlay);
+      removeDOMElement(paywallModal);
+    }
 
+    if (modalBackdrop) {
+      removeDOMElement(modalBackdrop);
+    }
+
+    if (loginLink) {
+      removeDOMElement(loginLink);
+    }
+
+    if (redactedOverlay) {
+      removeDOMElement(redactedOverlay);
+    }
+
+    if (subscriberOnly) {
       for (const el of subscriberOnly) {
         el.classList.remove('subscriber-only', 'hide');
-        el.classList.add('subscriber-preview')
+        el.classList.add('subscriber-preview');
       }
     }
 
