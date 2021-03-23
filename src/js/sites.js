a5dee28261c4dc335a7396442af75eb0cbd3dd91
Shaun Williams <shaun.williams@eangulus.com>
2021-03-23 13:55:59 +1100

    Update sites.js
    Added dailyliberal.com.au

--- src/js/sites.js
+++ src/js/sites.js
@@ -18,6 +18,7 @@ const defaultSites = {
   'Chicago Tribune': 'chicagotribune.com',
   'Corriere Della Sera': 'corriere.it',
   'Crain\'s Chicago Business': 'chicagobusiness.com',
+  'Daily Liberal': 'dailyliberal.com.au',
   'Daily Press': 'dailypress.com',
   'De Gelderlander': 'gelderlander.nl',
   'De Groene Amsterdammer': 'groene.nl',
